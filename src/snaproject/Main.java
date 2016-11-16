46d6340218ce7d494cab8fb20d189789fe6200d9
Remo Rossi <remo.rossi1@students.fhnw.ch>
2016-11-16 17:48:15 +0100

    asdf

--- src/snaproject/Main.java
+++ src/snaproject/Main.java
@@ -2,6 +2,8 @@ package snaproject;
 
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.json.JSONObject;
 
@@ -9,23 +11,39 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        String csvFile = "test.csv";
-        FileWriter writer = new FileWriter(csvFile);
-        ArrayList<JSONObject> list = new ArrayList<JSONObject>();
-        list = Movie.readFile("full_input.txt");
-
+        String csvFile1 = "nodes.csv";
+        String csvFile2 = "edges.csv";
         
-        //CSVUtils.writeLine(writer, Arrays.asList("aaa", "bb,b", "cc,c"), '\t');
-        CSVUtils.writeLine(writer, Movie.getTitles(list.get(0)), '\t');
+        FileWriter writer1 = new FileWriter(csvFile1);
+        FileWriter writer2 = new FileWriter(csvFile2);
+        
+        ArrayList<JSONObject> list = Movie.readFile("full_input.txt");
+
         for(JSONObject o : list) {
-        	if(o.length() == 20) CSVUtils.writeLine(writer, Movie.getAttributes(o), '\t');
+        	if(o.length() == 20) Movie.addAttributes(o);
         	else System.out.println("######################INVALID LENGTH###################");
         }
 
-
-
-        writer.flush();
-        writer.close();
+        //CSVUtils.writeLine(writer, Arrays.asList("aaa", "bb,b", "cc,c"), '\t');
+        CSVUtils.writeLine(writer1, Movie.getNodeTitles(), '\t');
+        for(List<String> l : Movie.getSetAsListOfLists(Movie.actorSet, 0)){
+        	CSVUtils.writeLine(writer1, l, '\t');
+        }
+        for(List<String> l : Movie.getSetAsListOfLists(Movie.genreSet, 1)){
+        	CSVUtils.writeLine(writer1, l, '\t');
+        }
+        
+        
+        CSVUtils.writeLine(writer2, Movie.getEdgeTitles(), '\t');
+        for(Edge e : Movie.edgeList){
+        	CSVUtils.writeLine(writer2, e.toStringList(), '\t');
+        }
+        
+        
+        writer1.flush();
+        writer1.close();
+        writer2.flush();
+        writer2.close();
 
     }
 
--- src/snaproject/Movie.java
+++ src/snaproject/Movie.java
@@ -5,32 +5,86 @@ import java.io.File;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 import org.json.JSONObject;
 
 public class Movie {
 	
+	static Set<String> actorSet = new HashSet<>();
+	static Set<String> genreSet = new HashSet<>();
+	static List<Edge> edgeList = new LinkedList<>();
 	
-	static ArrayList<String> getTitles(JSONObject o) {
-		Iterator<String> i = o.keys();
+	
+	static List<List<String>> getSetAsListOfLists(Set<String> set, int pos){
+		List<List<String>> listList = new ArrayList<List<String>>();
 		
-		ArrayList<String> list = new ArrayList<String>();
-		while(i.hasNext()) {
-			list.add(i.next());
+		for(String s : set){
+			List<String> list = new ArrayList<String>();
+			if(pos == 0){
+				list.add(s);
+				list.add("Actor");
+			}
+			else{
+				list.add(s);
+				list.add("Genre");
+			}
+			listList.add(list);
 		}
+		
+		return listList;
+	}
+	
+	
+	static ArrayList<String> getNodeTitles() {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("id");
+		list.add("type");
 		return list;
 	}
 	
-	static ArrayList<String> getAttributes(JSONObject o) {
-		Iterator<String> i = o.keys();
+	static ArrayList<String> getEdgeTitles() {
 		ArrayList<String> list = new ArrayList<String>();
-		while(i.hasNext()) {
-			String s = i.next();
-			list.add(o.getString(s));
-		}
+		list.add("source");
+		list.add("target");
+		list.add("movies");
+		list.add("imdbRating");
 		return list;
+	}
+	
+	static void addAttributes(JSONObject o) {
+		if(!"N/A".equals(o.getString("imdbRating"))){
+			String[] actors = o.getString("Actors").split(", ");
+			String[] genres = o.getString("Genre").split(", ");
+			float imdbRating = Float.valueOf(o.getString("imdbRating"));
+			
+			actorSet.addAll(Arrays.asList(actors));
+			genreSet.addAll(Arrays.asList(genres));
+			
+			boolean exists = false;
+			for(String actor : actors){
+				for(String genre : genres){
+					exists = false;
+					for(Edge e : edgeList){
+						if(e.equals(actor, genre)){
+							e.setMovies(e.getMovies()+1);
+							e.addImbdRating(imdbRating);
+							exists = true;
+							break;
+						}
+					}
+					if(!exists){
+						edgeList.add(new Edge(actor, genre, 1, imdbRating));
+					}
+				}
+			}
+		}
 	}
 	
 	static ArrayList<JSONObject> readFile(String path) throws Exception{
